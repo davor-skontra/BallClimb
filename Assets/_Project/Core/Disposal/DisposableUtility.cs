523672c119759a5006667d01e5f876af07c4ed10
theKrad23 <thekrad23@gmail.com>
2020-09-13 12:57:34 +0300

    Add Disposal classes

---
+++ Assets/_Project/Core/Disposal/DisposableUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Disposal
+{
+    public static class DisposableUtility
+    {
+        public static void Dispose(this IEnumerable<IDisposable> self)
+        {
+            foreach (var disposable in self)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
